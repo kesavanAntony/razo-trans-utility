f248390d8f6c909997cda2fff8b83b964ee5b2f3
kesavan <kesavan338@gmail.com>
2024-03-14 18:13:54 +0530

    hifi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react'
+import React, { Component, Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,13 +8,38 @@ const loading = (
   </div>
 )
 
+const lazyRetry = (componentImport) =>
+  new Promise((resolve, reject) => {
+    const storageKey = `retry-lazy-refreshed${btoa(componentImport.toString())}`;
+    const hasRefreshed = JSON.parse(window.sessionStorage.getItem(storageKey) || 'false');
+    componentImport()
+      .then((component) => {
+        window.sessionStorage.setItem(storageKey, 'false');
+        if (component === undefined) {
+          window.sessionStorage.setItem(storageKey, 'true');
+          return window.location.reload(); // refresh the page
+        }
+        resolve(component);
+      })
+      .catch((error) => {
+        if (!hasRefreshed) {
+          // not been refreshed yet
+          window.sessionStorage.setItem(storageKey, 'true');
+          window.location.reload();
+        }
+        reject(error); // Default error behaviour as already tried refresh
+      });
+  });
+
 // Containers
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+const DefaultLayout = React.lazy(() => lazyRetry(()=> import('./layout/DefaultLayout')))
 // Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const Login = React.lazy(() =>lazyRetry(() => import('./views/pages/login/Login')))
+const Register = React.lazy(() => lazyRetry (()=> import('./views/pages/register/Register')))
+const Page404 = React.lazy(() => lazyRetry (()=> import('./views/pages/page404/Page404')))
+const Page500 = React.lazy(() => lazyRetry (()=>mport('./views/pages/page500/Page500')))
+
+
 
 class App extends Component {
   render() {
